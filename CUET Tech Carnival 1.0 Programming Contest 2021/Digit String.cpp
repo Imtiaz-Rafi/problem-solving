1ba2df093b5b7097a34a67e0d2f48b840c2d9729
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2021-09-18 23:28:47 +0600

    digit string

---
+++ CUET Tech Carnival 1.0 Programming Contest 2021/Digit String.cpp
@@ -0,0 +1,24 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t,ts,a,i;
+    cin>>t;
+    for(ts=0;ts<t;ts++){
+        cout<<"Case "<<ts+1<<": ";
+        cin>>a;
+        for(i=0;i<a;i++){
+            cout<<0;
+        }
+        cout<<endl;
+    }
+
+
+
+return 0;// @Imtiaz_rafi
+}
