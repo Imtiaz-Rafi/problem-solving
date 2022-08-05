409985fc6b6f13ea00847d426cd3678da992e621
Imtiaz-Rafi <imtiazrafi199918@gmail.com>
2022-08-05 21:08:38 +0600

    bubble Sort Added

---
+++ preparation/BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main()
+{
+    ll a,i,j;
+    cin>>a;
+    ll b[a];
+    for(i=0;i<a;i++){
+        cin>>b[i];
+    }
+    for(i=0;i<a-1;i++){
+        for(j=0;j<a-i-1;j++){
+            if(b[j]>b[j+1]){
+                swap(b[j],b[j+1]);
+            }
+        }
+    }
+    for(i=0;i<a;i++){
+        cout<<b[i]<<" ";
+    }
+    cout<<endl;
+ 
+return 0;  //@Imtiaz_rafi
+}
